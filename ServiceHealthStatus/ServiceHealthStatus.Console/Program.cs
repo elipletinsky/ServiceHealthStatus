fabb8c24ca86030a00a1df413f873e92561f76c9
elipletinsky <eli.pletinsky@gmail>
2022-10-23 19:28:24 +0300

    added model and display json content in console

--- ServiceHealthStatus/ServiceHealthStatus.Console/Program.cs
+++ ServiceHealthStatus/ServiceHealthStatus.Console/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using ServiceHealthStatus.ViewModel;
+using System;
+using System.Collections;
+
+
+var services = await Service.Load(@"j:\temp\CatalogServiceModel.json");
+
+foreach (var service in services)
+{
+    Console.WriteLine($"sevice:");
+    Console.WriteLine($"name: {service.Name}");
+    Console.WriteLine($"environments:");
+    foreach (var environment in service.Environments)
+    {
+        Console.WriteLine($"\tname: {environment.Name}");
+        Console.WriteLine($"\tinstances:");
+        foreach (var instance in environment.Instances)
+        {
+            Console.WriteLine($"\t\t\tname: {instance.Name}");
+            Console.WriteLine($"\t\t\turl: {instance.Url}");
+        }
+    }
+}
